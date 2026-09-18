93a8c8ea22175c197ab3bd09f3ae25ba1c325942
Nyerhowo (Rob) Isuru <liquidityape@gmail.com>
2026-09-18 09:54:52 +0100

    update

---
+++ config.js
@@ -0,0 +1,23 @@
+// ===============================
+// Url Mini config
+// ===============================
+
+const SUPPORT_URL =
+  "https://t.me/noWalletConnect";
+
+// Link shortener
+// Database SHORTS with headers: date | code | url | note
+const SHORTS = {
+  SHEET_ID: "1ZUUmPDQGseg5bGEagOeFy89akZRtFQAIm5MksrGOrtw",
+  API_KEY: "AIzaSyCs8fSiITdfU4WyZRgKsWRlbhAKeJaVC0Q",
+  RANGE: "SHORTS!A:D",
+  GO_URL: "https://urlmini.github.io",
+  FORM: {
+    ACTION: "https://docs.google.com/forms/d/e/1r03FjjC_Jw4IH3n8I4_K5miPb6QiALPblCDDdjVrIA8/formResponse",
+    FIELDS: {
+                code: "entry.1136799663",
+                url: "entry.314328174",
+                user: "entry.1530086966"
+    }
+  }
+};
